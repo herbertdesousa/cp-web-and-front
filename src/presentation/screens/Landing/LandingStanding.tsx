4bcd689fe8c24db5439ca70a3bfb14ddb7a4c981
herbertdesousa <sousaherbert138@gmail.com>
2024-09-27 19:48:18 -0300

    feat(LandingStading): fix css

--- src/presentation/screens/Landing/LandingStanding.tsx
+++ src/presentation/screens/Landing/LandingStanding.tsx
@@ -4,7 +4,7 @@ const greatestPlayer = controllers.standing.GetGreatestPlayer();
 
 export function LandingStanding() {
   return (
-    <section className="section relative">
+    <section className="relative min-h-screen flex items-center justify-center">
       <div className="pt-16 flex gap-y-16 flex-col items-center">
         <h2 className="font-bold text-4xl uppercase text-center">
           Pódio da Mahindra

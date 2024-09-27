6956aaf486d50791b21e9adf13aa262945475af2
herbertdesousa <sousaherbert138@gmail.com>
2024-09-27 20:15:55 -0300

    feat(NewsController): impl

--- src/domain/Controllers.ts
+++ src/domain/Controllers.ts
@@ -1,10 +1,12 @@
 import { LocalStorageDatasource } from "../data/LocalStorageDatasource";
 import { InfluencersController } from "./InfluencersController";
+import { NewsController } from "./NewsController";
 import { StandingController } from "./StandingController";
 
-const localStorageDatasource = new LocalStorageDatasource()
+const localStorageDatasource = new LocalStorageDatasource();
 
 export const controllers = {
   standing: new StandingController(localStorageDatasource),
   influencer: new InfluencersController(localStorageDatasource),
+  news: new NewsController(localStorageDatasource),
 };
---
+++ src/domain/NewsController.ts
@@ -0,0 +1,41 @@
+import { LocalStorageDatasource } from "../data/LocalStorageDatasource";
+
+type News = {
+  id: string;
+  thumbnailUrl: string;
+  title: string;
+};
+
+export class NewsController {
+  constructor(private datasource: LocalStorageDatasource) {}
+
+  private storage = this.datasource.get("news");
+
+  // busca element em index e garante default value
+  private GetNewsAt(index: number, defaultValue: News) {
+    const storageNews = Array.isArray(this.storage) ? this.storage[index] : {};
+
+    return Object.assign(defaultValue, storageNews);
+  }
+
+  GetNews(): News[] {
+    return [
+      this.GetNewsAt(0, {
+        id: "id-1",
+        thumbnailUrl: "news-1.png",
+        title: "POLE POSITION: Participação na corrida de Marks Petter",
+      }),
+      this.GetNewsAt(1, {
+        id: "id-2",
+        thumbnailUrl: "news-2.png",
+        title: "‘I want to break free’ - Diz último ganhador do sorteio",
+      }),
+      this.GetNewsAt(2, {
+        id: "id-3",
+        thumbnailUrl: "news-3.png",
+        title:
+          "Champanhe e Festa: Influênciador e participante Julia são pego...",
+      }),
+    ];
+  }
+}
